e449cb8796b61c9b443bc9c102898b4be0ab6572
root <lahmar.kamel92@gmail.com>
2017-02-02 02:09:05 +0100

    feat(rpc_test - Auth): initial commit of the authentication tests file This file contain every test about the authentication module.

---
+++ AuthMSF_test.go
@@ -0,0 +1,43 @@
+package rpc_test
+
+import (
+	"testing"
+
+	. "github.com/gomsf"
+)
+
+var r = RPC{Host: "127.0.0.1", Port: 55553}
+
+func TestAuthLogin(t *testing.T) {
+	t.Log("Testing MSFRPC authentication")
+	rpcAuth := r.AuthLogin("kamel", "toto")
+	if rpcAuth.Error == true {
+		t.Errorf("rpcAuth authentication failed")
+	}
+	t.Logf("token received: ", r.Token)
+}
+
+func TestAuthTokenList(t *testing.T) {
+	t.Log("Testing MSFRPC token list")
+	rpcAuthTokenList := r.AuthTokenList()
+	t.Logf("token list: ", rpcAuthTokenList)
+}
+
+func TestAuthTokenAdd(t *testing.T) {
+	t.Log("Testing MSFRPC token add")
+	token := "newtoken"
+	rpcAuthTokenAdd := r.AuthTokenAdd(token)
+	if rpcAuthTokenAdd.MsfResponse == "success" {
+		t.Logf("Token: %s successfully added", token)
+	}
+	t.Errorf("rpcAuthTokenAdd failed")
+}
+
+func TestAuthTokenRemove(t *testing.T) {
+	t.Log("Testing MSFRPC token list")
+	rpcAuthTokenRemove := r.AuthTokenRemove(r.Token)
+	if rpcAuthTokenRemove.MsfResponse == "success" {
+		t.Logf("Token: %s successfully removed", r.Token)
+	}
+	t.Errorf("rpcAuthTokenRemove failed")
+}
